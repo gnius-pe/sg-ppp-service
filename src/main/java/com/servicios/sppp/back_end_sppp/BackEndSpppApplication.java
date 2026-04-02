e40ef1ed1f8541fee781d1fa33255f2163c3e3ce
igorrc <igor.ramos.c@hotmail.com>
2026-04-01 19:39:14 -0500

    feat: Enable Swagger2 documentation in main application

--- src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
+++ src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
@@ -2,8 +2,10 @@ package com.servicios.sppp.back_end_sppp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackEndSpppApplication {
 
 	public static void main(String[] args) {
