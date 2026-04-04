7247de6b8e35d7688cfc99fbfa6771bd1c60d290
igorrc <igor.ramos.cruzado.w@gmail.com>
2026-04-03 22:12:15 -0500

    chore: remove unused IOException import in ArchivoServicio

--- src/main/java/com/servicios/sppp/back_end_sppp/servicios/ArchivoServicio.java
+++ src/main/java/com/servicios/sppp/back_end_sppp/servicios/ArchivoServicio.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
