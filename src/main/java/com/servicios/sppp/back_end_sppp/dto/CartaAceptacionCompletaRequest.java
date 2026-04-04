e3bc4e416a7027dbdd6f71d1a2025763c554cc2e
igorrc <igor.ramos.cruzado.w@gmail.com>
2026-04-03 23:30:36 -0500

    feat(carta-aceptacion): add CartaAceptacionCompletaRequest DTO

---
+++ src/main/java/com/servicios/sppp/back_end_sppp/dto/CartaAceptacionCompletaRequest.java
@@ -0,0 +1,43 @@
+package com.servicios.sppp.back_end_sppp.dto;
+
+public class CartaAceptacionCompletaRequest {
+    private String titulo;
+    private String descripcion;
+    private Long idAlumno;
+    private String nombreEstado;
+
+    public CartaAceptacionCompletaRequest() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Long idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombreEstado() {
+        return nombreEstado;
+    }
+
+    public void setNombreEstado(String nombreEstado) {
+        this.nombreEstado = nombreEstado;
+    }
+}
