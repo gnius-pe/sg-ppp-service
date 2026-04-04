afd0c5b1aadbe774d8d904d3315e51f593fad3cd
igorrc <igor.ramos.cruzado.w@gmail.com>
2026-04-03 21:46:14 -0500

    chore: remove EnableSwagger2 annotation from main class

--- src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
+++ src/main/java/com/servicios/sppp/back_end_sppp/BackEndSpppApplication.java
@@ -2,15 +2,11 @@ package com.servicios.sppp.back_end_sppp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class BackEndSpppApplication {
 
 	public static void main(String[] args) {
-		//soy la rama develop
-		//soy la nueva api
 		SpringApplication.run(BackEndSpppApplication.class, args);
 	}
 
